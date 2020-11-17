2526ffc0658e39678cd533677ec10a5df72574e2
Javier <jf.javierfrauca@gmail.com>
2020-11-17 14:27:13 +0100

    Version

--- main/main.py
+++ main/main.py
@@ -5,6 +5,7 @@ from ota_updater import OTAUpdater
 def download_and_install_update_if_available():
     ota_updater = OTAUpdater('https://github.com/AliasJavier/ble_code.git')
     ota_updater.download_and_install_update_if_available('Acciona Innovacion', 'Innovacion_IoT')
+    ota_updater.check_for_update_to_install_during_next_reboot()
 
 def start():
    print("DOOOoa")
@@ -15,6 +16,7 @@ def boot():
 
 
 boot()
+
 
 
 
