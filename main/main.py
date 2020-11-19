b06e5b2b3f12ee180e0689381512d03a18cd8b8a
Aliasjavier <jf.javierfrauca@gmail.com>
2020-11-19 12:13:08 +0100

    primero

--- main/main.py
+++ main/main.py
@@ -1,35 +1,20 @@
 import machine
 from ota_updater import OTAUpdater
 
-def do_connect(): #Funcion para conectarse al wifi
-    import network
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    if not wlan.isconnected():
-        print('connecting to network...')
-        wlan.connect('Acciona Innovacion', 'Innovacion_IoT')
-        while not wlan.isconnected():
-            pass
-    else:
-        print('connected')
-    print('network config:', wlan.ifconfig())
-
 
 def download_and_install_update_if_available():
-    ota_updater = OTAUpdater('https://github.com/AliasJavier/ble_code')
-    ota_updater.check_for_update_to_install_during_next_reboot()
+    ota_updater = OTAUpdater('https://github.com/AliasJavier/ble_code.git')
     ota_updater.download_and_install_update_if_available('Acciona Innovacion', 'Innovacion_IoT')
 
 def start():
-   print("Se ha descargado")
+   print("DOOOOoaa")
 
 def boot():
     download_and_install_update_if_available()
     start()
 
-do_connect()
-boot()
 
+boot()
 
 
 
